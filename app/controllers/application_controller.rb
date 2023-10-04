5a40297ae31253dcbb50d4e49cf291782c82f834
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-10-04 11:19:24 -0400

    app controller updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies 
   rescue_from ActiveRecord::RecordNotFound, with: :no_route
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
-  before_action :authorized!
+  before_action :authorized! #will try to run on every action of every controller
 
-#   wrap_parameters format: [] to prevent nested object in params
+  wrap_parameters format: [] #to prevent nested object in params
 #  Rails will automatically try to package your parameters
 #  within the resource thats been generated. Dont want this duplicated so this macro can be used so object and object/data
 #  are both created
@@ -17,6 +17,9 @@ class ApplicationController < ActionController::API
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
+    # this is to find who the current user is
+    # ||= (memoization). Minimizes how many times it needs to go into the database. if not used 
+    #  the problem is it invokes .find multiple times.
 
     def authorized!
         no_route unless current_user
@@ -26,6 +29,9 @@ class ApplicationController < ActionController::API
         render json: {error: invalid.record.errors.full_messages.to_sentence}, status: :unprocessable_entity
     end
 
+    # def no_route
+    #     render json: {error: "Not authorized"}, status: :unauthorized unless session.include?(:user_id)
+    # end
     def no_route
         render json: {error: "Not authorized"}, status: :unauthorized unless session.include?(:user_id)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+skip_before_action :authorized!, only: [:create]
 
   def create
     user = User.create!(user_params) #the bang implies that there is no need for an if/else conditional
@@ -9,10 +9,13 @@ class UsersController < ApplicationController
 
   # the status isnt necessary but is good practice 
 
+  # show route is refactored here with changes made to application controller 
   def show
-    return render json: {error: "Not authorized"}, status: :unauthorized unless session.include?(:user_id)
-    user ||= User.find_by_id(session[:user_id])
-    render json: UserSerializer.new(user), status: :ok
+    # return render json: {error: "Not authorized"}, status: :unauthorized unless session.include?(:user_id)
+    # moved this since its already in application controller
+    # user ||= User.find_by_id(session[:user_id])
+    # this line not needed since running authorized up top which runs current user
+    render json: UserSerializer.new(@current_user), status: :ok
   end
   # this in charge of finding current user(if any) and happens at each page refresh
   # if session doesnt include user_id fronm first line, then fire next line
