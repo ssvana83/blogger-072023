e1ffe5049b5964111fc11cfc4e340a0ff00d2c60
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-10-11 11:52:06 -0400

    posts controller changes to index method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
+  skip_before_action :authorized!, only: [:index]
   before_action :find_post, only: [:show, :update, :destroy]
 
   def index #same as get "/posts"
-    render json: PostSerializer.new(Post.all).serializable_hash
+    render json: PostSerializer.new(Post.preload(:comments)).serializable_hash
   end
   # 
 
